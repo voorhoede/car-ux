78c8b7ae9ce28bf8c117f8e6933d9bc6807559b1
decrek <declanrek@gmail.com>
2014-09-19 16:59:16 +0200

    try fluffy

--- tasks/deploy-to-gh-pages.sh
+++ tasks/deploy-to-gh-pages.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 # build new distribution
-rm -rf distribution || exit 0;
+echo "cloning gh-pages"
+git clone -b gh-pages "https://${GH_TOKEN}@${GH_REF}" distribution > /dev/null 2>&1 || exit 1
+rm -rf distribution/* || exit 0
 npm install -g grunt-cli
+echo "building new distribution"
 grunt deploy
 # push it to gh-pages
 ( cd distribution
- git init
+ #git init
  git config user.name "Travis-CI"
  git config user.email "declanrek@gmail.com"
  git add .
  git commit -m "Deployed Car UX to Github Pages"
- # git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+ echo "push it real good"
+ #git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
