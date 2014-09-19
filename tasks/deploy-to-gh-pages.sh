619c77a7360af4c948a774145a2a41a5e566a19d
decrek <declanrek@gmail.com>
2014-09-19 16:15:27 +0200

    disable travis git

--- tasks/deploy-to-gh-pages.sh
+++ tasks/deploy-to-gh-pages.sh
@@ -5,10 +5,10 @@ npm install -g grunt-cli
 grunt deploy
 # push it to gh-pages
 ( cd distribution
- git init
- git config user.name "Travis-CI"
- git config user.email "travis@nodemeatspace.com"
- git add .
- git commit -m "Deployed Car UX to Github Pages"
- git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+ # git init
+ # git config user.name "Travis-CI"
+ # git config user.email "declanrek@gmail.com"
+ # git add .
+ # git commit -m "Deployed Car UX to Github Pages"
+ # git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
