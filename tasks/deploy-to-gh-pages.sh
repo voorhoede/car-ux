e11b36f3204cf944efb0f72947432c51b663f19f
decrek <declanrek@gmail.com>
2014-09-19 17:11:10 +0200

    travis clone, deploy, push

--- tasks/deploy-to-gh-pages.sh
+++ tasks/deploy-to-gh-pages.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 # build new distribution
+CURRENT_COMMIT=`git rev-parse HEAD`
+
 echo "cloning gh-pages"
 git clone -b gh-pages "https://${GH_TOKEN}@${GH_REF}" distribution > /dev/null 2>&1 || exit 1
 rm -rf distribution/* || exit 0
-npm install -g grunt-cli
+
 echo "building new distribution"
+npm install -g grunt-cli
 grunt deploy
+
 # push it to gh-pages
-( cd distribution
- #git init
- git config user.name "Travis-CI"
- git config user.email "declanrek@gmail.com"
- git add .
- git commit -m "Deployed Car UX to Github Pages"
- echo "push it real good"
- #git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-)
+cd distribution
+#git init
+echo "configure git user"
+git config user.name "Travis-CI"
+git config user.email "declanrek@gmail.com"
+echo "add and commit new distribution files"
+git add -A
+git commit -m "Deployed Car UX ($CURRENT_COMMIT) to Github Pages"
+echo "push it real good"
+git push origin gh-pages > /dev/null 2>&1
+#git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
