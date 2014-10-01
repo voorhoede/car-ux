937988497b17f48ec8fa0394e22ae9a288fc1ae4
Travis <travis@travis-ci.org>
2014-10-01 14:50:12 +0000

    Travis build 28 pushed to gh-pages

--- modules/components/detail-intro/_detail-intro.js
+++ modules/components/detail-intro/_detail-intro.js
@@ -1,23 +1,3 @@
-var helpers = helpers || {};
-
-/**
- * Debounce function - http://remysharp.com/2010/07/21/throttling-function-calls/
- *
- * @param fn
- * @param delay
- * @returns {Function}
- */
-function debounce(fn, delay) {
-	var timer = null;
-	return function () {
-		var context = this, args = arguments;
-		clearTimeout(timer);
-		timer = setTimeout(function () {
-			fn.apply(context, args);
-		}, delay);
-	};
-}
-helpers.debounce = debounce;
 (function(d, w){
 	'use strict';
 
