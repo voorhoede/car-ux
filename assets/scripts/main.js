cea2c2241384ef381692019a0ba6554e785d6578
Travis <travis@travis-ci.org>
2014-10-03 14:26:59 +0000

    Travis build 42 pushed to gh-pages

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -18,6 +18,74 @@ function debounce(fn, delay) {
 	};
 }
 helpers.debounce = debounce;
+/**
+ * Collection of global helpers
+ * @type {object} helpers
+ */
+var helpers = helpers || {};
+
+(function(){
+	'use strict';
+
+	/**
+	 * Collection of DOM helpers
+	 * @type {object} helpers.dom
+	 */
+	helpers.dom = helpers.dom || {};
+
+	/**
+	 * Borrowed from: http://hacks.mozilla.org/2010/01/classlist-in-firefox-3-6/
+	 * @param {HtmlElement} element
+	 * @param {String} className
+	 */
+	function addClass (element, className) {
+		if (document.documentElement.classList) {
+			element.classList.add(className);
+		} else {
+			if (!containsClass(element, className)) {
+				element.className += (element.className ? ' ' : '') + className;
+			}
+		}
+	}
+	helpers.dom.addClass = addClass;
+
+	/**
+	 * Borrowed from: http://hacks.mozilla.org/2010/01/classlist-in-firefox-3-6/
+	 * @param {HtmlElement} element
+	 * @param {String} className
+	 */
+	function removeClass (element, className) {
+		if (document.documentElement.classList) {
+			element.classList.remove(className);
+		} else {
+			var regexp = new RegExp('(^|\\s)' + className + '(\\s|$)', 'g');
+			element.className = element.className.replace(regexp, '$2');
+		}
+	}
+	helpers.dom.removeClass = removeClass;
+
+	/**
+	 * Borrowed from: http://hacks.mozilla.org/2010/01/classlist-in-firefox-3-6/
+	 * @param {HtmlElement} element
+	 * @param {String} className
+	 */
+	function toggleClass (element, className){
+		if (document.documentElement.classList) {
+			return element.classList.toggle(className);
+		} else {
+			if (containsClass(element, className))
+			{
+				removeClass(element, className);
+				return false;
+			} else {
+				addClass(element, className);
+				return true;
+			}
+		}
+	}
+	helpers.dom.toggleClass = toggleClass;
+
+}());
 var helpers = helpers || {};
 
 (function(d, w){
@@ -405,3 +473,17 @@ var helpers = helpers || {};
 	new window.Expandible();
 
 })(document);
+(function(d){
+	'use strict';
+
+	var toggleClass = helpers.dom.toggleClass;
+	var widgetElms = d.querySelectorAll('[data-component="widget-score"]');
+
+	[].forEach.call(widgetElms, function (widgetElm) {
+		var headerElm = widgetElm.querySelector('[data-widget-score-overview]');
+		headerElm.addEventListener('click', function() {
+			toggleClass(this, 'expanded');
+		});
+	});
+
+}(document));
