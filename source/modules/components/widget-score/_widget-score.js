cd12aae7e032eb8fa6fbdf0ba73178f80f17e945
Jeroen Dijkstra <jeroen@voorhoede.nl>
2014-10-07 15:39:29 +0200

    Update styling intro score widget and add logo to header

--- source/modules/components/widget-score/_widget-score.js
+++ source/modules/components/widget-score/_widget-score.js
@@ -4,10 +4,14 @@
 	var toggleClass = helpers.dom.toggleClass;
 	var widgetElms = d.querySelectorAll('[data-component="widget-score"]');
 
+
 	[].forEach.call(widgetElms, function (widgetElm) {
 		var headerElm = widgetElm.querySelector('[data-widget-score-overview]');
+		var headerArrow = widgetElm.querySelector('[data-score-header-arrow]');
 		headerElm.addEventListener('click', function() {
 			toggleClass(this, 'expanded');
+			toggleClass(headerArrow, 'icon-arrow-down');
+			toggleClass(headerArrow, 'icon-arrow-up');
 		});
 	});
 
