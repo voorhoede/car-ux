5a84e1524650a2f0a6f16f2a59a1e0a6f245cb8f
Travis <travis@travis-ci.org>
2014-10-09 11:14:08 +0000

    Travis build 95 pushed to gh-pages

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -514,6 +514,16 @@ var helpers = helpers || {};
 		var headerElm = widgetElm.querySelector('[data-widget-score-overview]');
 		var headerArrow = widgetElm.querySelector('[data-score-header-arrow]');
 		headerElm.addEventListener('click', function() {
+
+			var anchors = this.querySelectorAll('a');
+			[].forEach.call([].slice.call(anchors), function(anchor) {
+				if (anchor.getAttribute('tabindex') === '-1') {
+					anchor.setAttribute('tabindex', '0');
+				} else {
+					anchor.setAttribute('tabindex', '-1');
+				}
+			});
+
 			toggleClass(this, 'expanded');
 			toggleClass(headerArrow, 'icon-arrow-down');
 			toggleClass(headerArrow, 'icon-arrow-up');
