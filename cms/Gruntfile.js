350146f2d4604652fdddde78587d65120a9090d9
Sanne Veroude <sanne@voorhoede.nl>
2015-03-18 13:15:40 +0100

    feat(cms) add grunticon

--- cms/Gruntfile.js
+++ cms/Gruntfile.js
@@ -15,10 +15,41 @@ module.exports = function(grunt) {
   //
   // You have been warned!
   grunt.initConfig({
-
+    grunticon: {
+      foo: {
+        files: [
+            {
+                expand: true,
+                cwd: 'static/images/grunticon-svgs',
+                src: ['*.svg', '*.png'],
+                dest: 'static/images/grunticon-output'
+            }
+        ],
+        options: {
+            customselectors: {
+                'left-red' : ['.icon-left:hover', '.icon-left:focus'],
+                'facebook-yellow-white': ['.icon-facebook:hover', '.icon-facebook:focus'],
+                'gplus-yellow-white': ['.icon-gplus:hover', '.icon-gplus:focus'],
+                'twitter-yellow-white': ['.icon-twitter:hover', '.icon-twitter:focus']
+            },
+            prefix: {
+                prefix: '.icon-'
+            },
+          colors: {
+                red: '#CD4D4D',
+                yellow: '#fee165'
+          },
+            defaultWidth: '30px',
+            defaultHeight: '30px'
+        }
+      }
+    }
   });
 
   // NEVER REMOVE THESE LINES, OR ELSE YOUR PROJECT MAY NOT WORK
   require('./options/generatorOptions.js')(grunt);
   grunt.loadTasks('tasks');
+
+  grunt.loadNpmTasks('grunt-grunticon');
+
 };
